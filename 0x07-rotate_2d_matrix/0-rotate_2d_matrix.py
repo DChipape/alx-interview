8387fa5d061ffa63f69720fb9545840ab897eafd
dchipape <dimakatsosharon94@gmail.com>
2024-06-04 15:22:45 -0700

    task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,7 +2,6 @@
 """
 Rotate 2D Matrix
 """
-
 def rotate_2d_matrix(matrix):
     """
     Rotate the given n x n 2D matrix 90 degrees clockwise in place.
@@ -12,23 +11,19 @@ def rotate_2d_matrix(matrix):
     """
     if not isinstance(matrix, list):
         raise TypeError("Input matrix must be a list")
-
     n = len(matrix)
-
     # Step 1: Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # Step 2: Reverse each row
+            # Step 2: Reverse each row
     for i in range(n):
         matrix[i].reverse()
 
-
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
+        if __name__ == "__main__":
+            matrix = [[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]]
 
     rotate_2d_matrix(matrix)
     print(matrix)
